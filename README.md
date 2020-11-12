64cabee5f508a6e630feebf922deefe82610e09d
Wagura Maurice <wagura465@gmail.com>
2020-11-12 19:35:32 +0300

    BOOTSTRAP 5 LARAVEL FORTIFY AUTHENTICATION

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center"><a href="https://waguramaurice.com" target="_blank">BOOTSTRAP 5 LARAVEL FORTIFY AUTHENTICATION</a></h1>
+
+This is a simple auth starter setup for laravel 8 projects
+
+## Features
+
+-   [x] User Login
+-   [x] User Registration
+-   [x] Email Verification
+-   [x] Forget Password
+-   [x] Reset Password
+-   [ ] Change Password
+-   [ ] Update User Profile
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-3">Bootstrap 5 Laravel Fortify Authentication</h1>
+        <p class="lead">This is a simple auth starter setup for laravel 8 projects</p>
+        <hr class="my-4">
+        <h2>Features:</h2>
+        <ul>
+            <li>User Login</li>
+            <li>User Registration</li>
+            <li>Email Verification</li>
+            <li>Forget Password</li>
+            <li>Reset Password</li>
+        </ul>
+        <p class="lead">
+            <a class="btn btn-primary" href="" target="_blank" role="button">Github Source Code</a>
+        </p>
+    </div>
+</div>
+@endsection
